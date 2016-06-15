28ec9931fd0eabdc35e05909193d4cc779de4706
Mike O'Donnell <mike.odonnell@hybris.com>
2016-06-14 18:05:29 -0600

    use gulp, angular bootstrap, gulp-less, and gulp-connect

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    connect = require('gulp-connect');
+
+gulp.task('build-less', function(){
+    return gulp.src('less/index.less')
+        .pipe(less())
+        .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./less/*.less', ['less']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: ['app'],
+        port: 8000,
+        livereload: true
+    });
+});
+
+gulp.task('default', [
+    'build-less',
+    'connect',
+    'watch'
+]);
